83b301f2c79ea6c71b62d51e6910bd7501c60962
Inga <ijaanimets@gmail.com>
2020-09-23 22:13:44 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,22 @@
 # Write your methods here
+def loop_message_five_times(string)
+ 5.times do
+   puts string
+ end
+end
+
+def loop_message_n_times(string, integer)
+  integer.times do
+    puts string
+  end
+end
+
+def output_array(array)
+  array.each do |i|
+    puts i
+  end
+end
+
+def return_string_array(array)
+  array.map { |i| array << i.to_s }
+end
